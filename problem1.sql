8f758034d67ad3b573d7c5172f62b4c39c66e574
Taikun Guo <billg1990@gmail.com>
2017-11-12 21:55:58 -0500

    Added postgres queries.

--- problem1.sql
+++ problem1.sql
@@ -1,6 +1,6 @@
 -- Homework for class Advanced Database Systems
 -- Copyright 2017 Taikun Guo and Yi Zhang
--- following code are tested in MySQL 5.7.20 and PostgreSQL
+-- following code are tested in my local PostgreSQL
 
 -- a. Find the weighted (by quantity) average price of each stock over the entire time series.
 SELECT
@@ -8,90 +8,54 @@ SELECT
   (SUM(quantity*trade.price)/SUM(quantity)) AS average_price
 FROM trade
 GROUP BY stocksymbol;
--- Compare:
--- MySQL 70002 rows in set (10.22 sec)
--- Postgre 
+-- time taken 3s140ms 
 
 -- b. Find the vector of 10 trade unweighted moving averages (i.e. moving average of price) per stock.
-SELECT
-  t2.stocksymbol,
-  GROUP_CONCAT(( -- concat them into a vector
-    -- Internal table to query the average of
-    -- 10 records of the same stocksymbol
-    SELECT
-      AVG(t1.price)
-    FROM
-      trade t1
-    WHERE
-      t1.stocksymbol = t2.stocksymbol AND t1.time >= t2.time
-    GROUP BY t1.stocksymbol LIMIT 10
-  ) SEPARATOR ', ') AS vector
-FROM
-  trade t2
+SELECT t2.stocksymbol, array_agg(t2.avg_prices) AS unweighted_avg
+FROM (
+  SELECT t.stocksymbol,
+    AVG(t.price)
+    OVER(
+      PARTITION BY t.stocksymbol
+      ORDER BY t.time ROWS BETWEEN CURRENT ROW AND 10 FOLLOWING
+      ) AS avg_prices
+  FROM trade t
+) t2
 GROUP BY t2.stocksymbol;
--- Compare:
--- MySQL 70002 rows in set (10.22 sec)
--- Postgre 
+-- time taken 19s450ms
 
 -- c. Find the vector of 10 trade weighted moving averages per stock.
-SELECT
-  t2.stocksymbol,
-  GROUP_CONCAT((
-    SELECT
-      -- weighted
-      SUM(t1.price*t1.quantity)/SUM(t1.quantity)
-    FROM
-      trade t1
-    WHERE
-      t1.stocksymbol = t2.stocksymbol AND t1.time >= t2.time
-    GROUP BY t1.stocksymbol LIMIT 10
-  ) SEPARATOR ', ') AS vector
-FROM
-  trade t2
+SELECT t2.stocksymbol, array_agg(t2.total_prices/t2.quantities) AS weighted_avg
+FROM (
+  SELECT t.stocksymbol,
+    AVG(t.price*t.quantity)
+    OVER(
+      PARTITION BY t.stocksymbol
+      ORDER BY t.time ROWS BETWEEN CURRENT ROW AND 10 FOLLOWING
+      ) AS total_prices,
+    AVG(t.quantity)
+    OVER(
+      PARTITION BY t.stocksymbol
+      ORDER BY t.time ROWS BETWEEN CURRENT ROW AND 10 FOLLOWING
+      ) AS quantities
+  FROM trade t
+) t2
 GROUP BY t2.stocksymbol;
--- Compare:
--- MySQL 70002 rows in set (10.22 sec)
--- Postgre 
+-- time taken 26s160ms
 
 -- d. Find the single best buy first/sell later trade you could have done on each stock (your
 -- query should work on our data as well as yours). That is, for each stock, find the
 -- maximum positive price difference between a later sell and an earlier buy.
-SELECT
-  t3.stocksymbol,
-  (
-    SELECT
-      MAX(t2.price - t1.price)
-    FROM trade t1 CROSS JOIN trade t2
-    -- t2 is later sell so t2.time should be bigger than t1.time
-    WHERE t1.stocksymbol = t3.stocksymbol AND t2.stocksymbol = t3.stocksymbol AND t1.time < t2.time
-    GROUP BY t1.stocksymbol
-  ) AS best -- best trade for a stock
-FROM
-  trade t3
-GROUP BY t3.stocksymbol;
--- Compare:
--- MySQL 70002 rows in set (10.22 sec)
--- Postgre 
-
-
-
-
-
-
-SELECT
-  t2.stocksymbol,
-  GROUP_CONCAT(( -- concat them into a vector
-    -- Internal table to query the average of
-    -- 10 records of the same stocksymbol
-    SELECT
-      AVG(t1.price)
-    FROM
-      trade t1
-    WHERE
-      t1.stocksymbol = t2.stocksymbol AND t1.time >= t2.time
-    GROUP BY t1.stocksymbol LIMIT 10
-  ) SEPARATOR ', ') AS vector
-FROM
-  trade t2
-WHERE t2.stocksymbol = 1
-GROUP BY t2.stocksymbol;
+SELECT t1.stocksymbol,MAX(t1.diff)
+FROM (
+  SELECT t.stocksymbol,
+    t.price - MIN(t.price)
+    OVER (
+      PARTITION BY t.stocksymbol
+      ORDER BY t.time
+      ROWS UNBOUNDED PRECEDING
+      ) AS diff
+  FROM trade t
+) t1
+GROUP BY t1.stocksymbol;
+-- time taken 11s180ms
